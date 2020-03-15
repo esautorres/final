8e9a18056d9a485115c9c4480dfd52d911c79be5
esautorres <esau.torres@kellogg.northwestern.edu>
2020-03-15 22:47:49 +0000

    final_v3

--- createdb.rb
+++ createdb.rb
@@ -12,6 +12,8 @@ DB.create_table! :events do
   String :date
   String :location
   String :price
+  String :long
+  String :lat
 end
 DB.create_table! :rsvps do
   primary_key :id
@@ -36,31 +38,41 @@ events_table.insert(title: "Coachella",
                     description: "The Coachella Valley Music and Arts Festival is an annual music and arts festival held at the Empire Polo Club.",
                     date: "April 10-12",
                     location: "Indio, CA", 
-                    price: "$1,000")
+                    price: "$1,000",
+                    long: "33.678368", 
+                    lat: "-116.239371")
 
 events_table.insert(title: "EDC", 
                     description: "EDC is the largest electronic dance music festival in North America.",
                     date: "May 15-17",
                     location: "Las Vegas, NV",
-                    price: "$925")
+                    price: "$925", 
+                    long: "36.2721997", 
+                    lat: "-115.0122888")
 
 events_table.insert(title: "Outside Lands", 
                     description: "The Outside Lands Music and Arts Festival is a music festival featuring a diverse selection of artists.",
                     date: "August 7-9",
                     location: "San Francisco, CA", 
-                    price: "$900")
+                    price: "$900",
+                    long: "37.7671962", 
+                    lat: "-122.4927452")
 
 events_table.insert(title: "Stagecoach", 
                     description: "The Stagecoach Festival is an outdoor country music festival held at the Empire Polo Club.",
                     date: "April 24-26",
                     location: "Indio, CA",
-                    price: "$975")
+                    price: "$975",
+                    long: "33.678368", 
+                    lat: "-116.239371")
 
 events_table.insert(title: "Ultra", 
                     description: "Ultra Music Festival is an annual outdoor electronic music festival featuring some of the best DJ's in the world.",
                     date: "March 20-21",
                     location: "Miami, FL", 
-                    price: "$1,125")
+                    price: "$1,125",
+                    long: "25.7744356", 
+                    lat: "-80.1895185")
 
 
 puts "Success"
