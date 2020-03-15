776518132294dda8ee4bccd3ced195b98986bd40
esautorres <esau.torres@kellogg.northwestern.edu>
2020-03-15 13:31:26 +0000

    final_v1

--- app.rb
+++ app.rb
@@ -16,3 +16,149 @@ after { puts; }                                                                 
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end 
+
+get "/" do
+    puts "params: #{params}"
+
+    pp events_table.all.to_a
+    @events = events_table.all.to_a
+    view "home_page"
+end
+
+get "/events" do
+    puts "params: #{params}"
+    
+    
+    pp events_table.all.to_a
+    @events = events_table.all.to_a
+    view "events"
+end
+
+get "/events/:id" do
+    puts "params: #{params}"
+    
+    @users_table = users_table
+    @event = events_table.where(id: params[:id]).to_a[0]
+    pp @event
+    @rsvps = rsvps_table.where(event_id: @event[:id]).to_a
+    @going_count = rsvps_table.where(event_id: @event[:id]).count
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    puts "params: #{params}"
+
+    @event = events_table.where(id: params[:id]).to_a[0]
+
+    @user_logged_in = users_table.where(id: session["user_id"]).to_a[0] 
+    if @user_logged_in 
+            #know the user is logged in 
+            view "new_rsvp"
+        else
+            #force user to log in before being able to purchase
+            view "new_login"
+        end
+end
+
+get "/events/:id/rsvps/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for 
+    @event = events_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvp 
+    rsvps_table.insert(
+        event_id:@event[:id],   # need to associate what event we are rsvp'ing for, thus we add 'event id'
+        user_id: session["user_id"],
+        billing_name: params["billing_name"],
+        billing_address: params["billing_address"],
+        comments: params["comments"], 
+        going: params["going"], 
+        cc_number: BCrypt::Password.create(params["cc_number"])
+    )
+    view "create_rsvp"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts "params: #{params}"
+    # if there's already a user with this email, skip (week 10 in-class):
+    #existing_user = users.table.where(email: params["email"]).to_a[0]
+    #if existing_user
+       # view "error"
+    #else
+    #week 9 in-class    
+    users_table.insert(         #this inserts the data that is input into the 'new_user.erb' 
+        #note that no 'id' is required, since no association is required
+        name: params["name"],
+        email: params["email"], 
+        password: BCrypt::Password.create(params["password"])
+    )
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    puts "params: #{params}"
+    
+    # first, is there a user with the params["email"]
+    # second, if there is, does the password match?
+    @user = users_table.where(email: params["email"]).to_a[0] 
+    if @user && BCrypt::Password.new(@user[:password]) == params["password"]
+            #know the user is logged in 
+            session["user_id"] = @user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+end
+
+get "/logout" do
+    session["user_id"] = nil #set this to remove cookies and log user out
+    view "logout"
+end
+
+#week 10 in-class 
+get "/rsvps/:id/edit" do
+    puts "params: #{params}"
+
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    view "edit_rsvp"
+end
+
+post "/rsvps/:id/update" do
+    puts "params: #{params}"
+
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    #if @current_user && @current_user[:id] == @rsvp[:id]
+    #rsvps_table.where(id:params["id"]).update(
+        #going: params["going"],
+       # comments: params["comments"]
+   # )
+   # view "update_rsvp"
+    #else
+    #view "error"
+end
+
+get "/rsvps/:id/destroy" do
+    puts "params: #{params}"
+
+    rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @event = events_table.where(id: rsvp[:event_id]).to_a[0]
+
+    rsvps_table.where(id: params["id"]).delete
+
+    view "destroy_rsvp"
+end
--- createdb.rb
+++ createdb.rb
@@ -11,25 +11,63 @@ DB.create_table! :events do
   String :description, text: true
   String :date
   String :location
+  String :price
+  String :img_url
 end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+  String :billing_name
+  String :billing_address
+  String :cc_number
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Coachella", 
+                    description: "The Coachella Valley Music and Arts Festival is an annual music and arts festival held at the Empire Polo Club.",
+                    date: "April 10-12",
+                    location: "Indio, CA", 
+                    price: "$1,000",
+                    img_url: "https://drive.google.com/open?id=1hAQUmZ7eo3rUjUEIJH8zg8jA3tic-NG3")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "EDC", 
+                    description: "EDC is the largest electronic dance music festival in North America.",
+                    date: "May 15-17",
+                    location: "Las Vegas, NV",
+                    price: "$925",
+                    img_url: "https://drive.google.com/open?id=13scCjWZ4Vm_LvtVPfzK2T01cEQlMZXWx")
+
+events_table.insert(title: "Outside Lands", 
+                    description: "The Outside Lands Music and Arts Festival is a music festival featuring a diverse selection of artists.",
+                    date: "August 7-9",
+                    location: "San Francisco, CA", 
+                    price: "$900",
+                    img_url: "https://drive.google.com/open?id=17fkyI_LW7MmBCudU4yZd5eiq6umtm5Wp")
+
+events_table.insert(title: "Stagecoach", 
+                    description: "The Stagecoach Festival is an outdoor country music festival held at the Empire Polo Club.",
+                    date: "April 24-26",
+                    location: "Indio, CA",
+                    price: "$975",
+                    img_url: "https://drive.google.com/open?id=1UvLi7FzA_xKPx6mTR97WR0Uqzfb3_wIu")
+
+events_table.insert(title: "Ultra", 
+                    description: "Ultra Music Festival is an annual outdoor electronic music festival featuring some of the best DJ's in the world.",
+                    date: "March 20-21",
+                    location: "Miami, FL", 
+                    price: "$1,125", 
+                    img_url: "https://drive.google.com/open?id=1ioCZVpY6csOSYQVAdJkKh6HGZpL1P7oj")
+
+
+puts "Success"
